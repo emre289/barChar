fb73ccc1605353ee1d21e2fc6cec7ae7c358ab20
Emre Demirbaş <emre289@gmail.com>
2021-03-24 18:13:22 +0300

    bar char added.

---
+++ app/src/main/java/com/emre/barchar/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.emre.barchar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setBarChart()
+
+    }
+    private fun setBarChart() {
+        val entries = ArrayList<BarEntry>()
+        entries.add(BarEntry(8f, 0))
+        entries.add(BarEntry(2f, 1))
+        entries.add(BarEntry(5f, 2))
+        entries.add(BarEntry(20f, 3))
+        entries.add(BarEntry(15f, 4))
+        entries.add(BarEntry(19f, 5))
+
+        val barDataSet = BarDataSet(entries, "DURUM")
+
+        val labels = ArrayList<String>()
+        labels.add("18-Pzt")
+        labels.add("19-Sal")
+        labels.add("20-Çar")
+        labels.add("21-Per")
+        labels.add("22-Cum")
+        labels.add("23-Cts")
+        val data = BarData(labels, barDataSet)
+        barChart.data = data // set the data and list of lables into chart
+        
+        barChart.setDescription("Set Bar Chart Description")  // set the description
+
+        //barDataSet.setColors(ColorTemplate.COLORFUL_COLORS)
+        barDataSet.color = resources.getColor(R.color.black)
+
+        barChart.animateY(5000)
+    }
+
+}
